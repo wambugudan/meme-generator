adfbf8d1cb56698c942ae065f2865328cbcab86a
Daniel Wambugu <wambugudani@gmail.com>
2024-12-09 23:15:57 +0300

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div className="header">
+            <img src="meme-logo.png" alt="logo" className="meme-logo" />
+            <h1>Meme Generator</h1>
+        </div>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+
+export default function Main(){
+
+    const obj = {
+        top_text: "",
+        bottom_text: "Si pite",
+        imgUrl: "meme1.jpg",
+    }
+
+    const [memeItem, setMemeItem] = useState(obj)
+
+    const handleChange = function(event){
+        const {value} = event.currentTarget
+        setMemeItem(prevState => ({
+            ...prevState,
+            top_text: value,
+        }))
+    }
+
+    return(
+        <div className="meme-body-container">
+            <form action="">
+                <div className="inputs">
+                    <label htmlFor="top-text">Top Text</label>
+                    <input type="text" value={memeItem.top_text} placeholder="Yaliyo ndwele" name="top-text" onChange={handleChange} />
+                </div>
+
+                <div className="inputs">
+                    <label htmlFor="bottom-text">Botton Text</label>
+                    <input type="text" placeholder="Si pite" name="bottom-text" />
+                </div>
+            </form>
+
+            <div className="btn">
+                <button>Get New Meme Image</button>
+            </div>
+
+            <div className="meme-image-container">
+                <img src={memeItem.imgUrl} alt="" />
+                <span className="top-text">{memeItem.top_text}</span>
+                <span className="bottom-text">{memeItem.bottom_text}</span>
+            </div>
+        </div>
+    )
+}
